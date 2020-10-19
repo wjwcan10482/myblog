9c42962e813477e0b86c520397e0554c52ff39de
wangjin <wjwcan10482@163.com>
2020-10-19 11:01:01 +0800

    update urlconvert

--- myblog/urlconvert/views.py
+++ myblog/urlconvert/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import requests
 @csrf_exempt
 def index(request):
     return render(request, 'urlconvert/index.html')
 
 def convert(request):
     request.encoding='utf-8'
-    print(request.body)
-    print(request.body)
     if request.method == 'POST':
         if request.POST:
-            print("POST OK!!!!!!!")
-            return HttpResponse("POST OK!!!!!!!")
+            sourceurl = str(request.body, encoding = "utf-8")
+            print(sourceurl)
+            print(sourceurl.split('/')[-1])
+            sourcesoftname= sourceurl.split('/')[-1]
+            r = requests.get(sourceurl)
+            with open("/usr/share/nginx/html/soft/"+sourcesoftname, "wb") as code:
+                code.write(r.content)
+            return HttpResponse("POST OK!!!")
         else:
             print("POST Failed!!!!!!!")
             return HttpResponse("POST Failed!!!!!!!")
